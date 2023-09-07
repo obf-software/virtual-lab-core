0da374033b58fc9352ba39fe7af8ebbc8a3e2163
Otávio Baziewicz Filho <otavio.baziewicz.filho@gmail.com>
2023-09-07 13:29:35 -0300

    wip: Add chakra-ui

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { App } from './App.tsx';
+import { ChakraProvider } from '@chakra-ui/react';
+import { theme } from './styles/theme';
+
+import './styles/global.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
-        <App />
+        <ChakraProvider theme={theme}>
+            <App />
+        </ChakraProvider>
     </React.StrictMode>,
 );
